7cca056207b7c8055193ac6c60922f59d39a1b6f
Venopsis <949573+Venopsis@users.noreply.github.com>
2019-06-30 19:56:15 +0200

    Add web support
    To prevent app to crash on Heroku hosting

--- bot.js
+++ bot.js
@@ -15,4 +15,11 @@ client.on('message', msg => {
     }
   });
 
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(443);
+
 client.login(auth.token);
