80ef5e5571ad0f465eddd345003f35258d137e6b
venopsis <949573+Venopsis@users.noreply.github.com>
2019-06-30 18:40:08 +0200

    test

---
+++ bot.js
@@ -0,0 +1,18 @@
+//https://www.digitaltrends.com/gaming/how-to-make-a-discord-bot/
+//https://medium.com/davao-js/2019-tutorial-creating-your-first-simple-discord-bot-47fc836a170b
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('pong');
+    }
+  });
+
+client.login(auth.token);
