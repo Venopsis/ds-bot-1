389dbd3e000b257446e8bd052243bd5080fab4b6
Venopsis <949573+Venopsis@users.noreply.github.com>
2019-07-01 21:08:41 +0200

    Removed web server

--- bot.js
+++ bot.js
@@ -15,11 +15,4 @@ client.on('message', msg => {
     }
   });
 
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(443);
-
 client.login(auth.token);
