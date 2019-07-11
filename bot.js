ea94727a06bd7dc7424cd8e1a6758015fbc6127b
Venopsis <949573+Venopsis@users.noreply.github.com>
2019-07-11 19:59:35 +0200

    //WIP - Prune people

--- bot.js
+++ bot.js
@@ -14,8 +14,11 @@ BOT.on('message', msg => {
     if (msg.content === '!prune') {
       GUILD.pruneMembers(15,true)
       .then(pruned => console.log(`I just pruned ${pruned} people!`))
-      .then(pruned => console.log(`Following people will be pruned : ${pruned.user}`))
+      //.then(pruned => console.log(`Following people will be pruned : ${pruned.user}`))
       .catch(console.error);
+    }
+    else{
+      console.log("Not a recognized command!")
     }
   });
 
